646e09c439e3ae6af343f4a68c0043ec0309a3e0
Arafat Hossain Ar <arafat.122260@gmail.com>
2021-08-10 00:25:35 +0600

    first version completed

--- src/App.js
+++ src/App.js
@@ -10,17 +10,119 @@ function App() {
 
       <div className="section">
         <div className="row">
-          <div className="col-2">
+          <div className="col-3">
             <div className="card">
               <div className="title">
+                Lorem ipsum dolor sit amet consectetur. →
+              </div>
 
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi.......
               </div>
             </div>
           </div>
-          <div className="col-2">
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
 
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi......
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
+
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi......
+              </div>
+            </div>
           </div>
         </div>
+
+
+        <div className="row">
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
+
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi.......
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
+
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi......
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
+
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi......
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
+
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi.......
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
+
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi......
+              </div>
+            </div>
+          </div>
+          <div className="col-3">
+            <div className="card">
+              <div className="title">
+                Lorem ipsum dolor sit amet consectetur.  →
+              </div>
+
+              <div className="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad modi reiciendis doloremque sint ullam cupiditate nemo nobis autem mollitia? Nisi......
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="btn">
+          Load More
+          </div>
+      </div>
+      <div className="footer">
+        <p className="text"> All right Reserved By <a href="https://arafat-web.github.io">Arafat</a></p>
       </div>
     </div>
   );
